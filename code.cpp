6a1640c0557ae1f04868320ee83903daef7e758b
codewithsaidul <codewithsaidul@gmail.com>
2025-08-07 13:33:28 +0600

    a.out file removed

---
+++ code.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main () {
+    int a;
+    int b;
+
+    cout << "Enter value a: " << endl;
+    cin >> a;
+    cout << "Enter value b: " << endl;
+    cin >> b;
+    int c = a + b;
+    cout << "Your sum is: " << c << endl;
+    return 0;
+}
